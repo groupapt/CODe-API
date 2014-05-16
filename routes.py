537f581543e4b212c9343f05f26f4d8ef48bd8dd
Daniel <desiradaniel2007@hotmail.com>
2014-05-16 17:33:06 +0200

    Remove town route

--- routes.py
+++ routes.py
@@ -12,6 +12,7 @@ def query_db(query_string):
 	graph_db = neo4j.GraphDatabaseService()
 	return neo4j.CypherQuery(graph_db, query_string).execute()
 
+
 def form_json(result):
 	dataset = {'response': []}
 	for record in result:
@@ -21,22 +22,23 @@ def form_json(result):
 		dataset['response'].append(node)
 	return jsonify(dataset)
 
+
 def query_cases_by_role(role, name, surname):
-	query_string = 'MATCH (case) WHERE case.' + role + ' =~ \'' + name + '.*\' AND case.' + role + ' =~ \'.*' + surname + '.*\' RETURN case'
-	result = query_db(query_string)
+	query_str = 'MATCH (case) WHERE case.' + role + ' =~ "' + name + '.*" AND case.' + role + ' =~ ".*' + surname + '.*" RETURN case'
+	result = query_db(query_str)
 	if result == None:
-		query_string = 'MATCH (case) WHERE case.' + role + ' =~ \'' + name + '.*\' OR case.' + role + ' =~ \'.*' + surname + '.*\' RETURN case'
-		result = query_db(query_string)
+		query_str = 'MATCH (case) WHERE case.' + role + ' =~ "' + name + '.*" OR case.' + role + ' =~ ".*' + surname + '.*" RETURN case'
+		result = query_db(query_str)
 	return result
 
 @app.route('/case/<reference_p1>/<reference_p2>')
 def case(reference_p1, reference_p2):
-	result = query_db('MATCH (case) WHERE case.reference = \'' + reference_p1 + '/' + reference_p2 + '\' RETURN case LIMIT 1')
+	result = query_db('MATCH (case) WHERE case.reference = "' + reference_p1 + '/' + reference_p2 + '" RETURN case LIMIT 1')
 	return form_json(result)
 
 @app.route('/cases/year/<int:year>')
 def year_cases(year):
-	result = query_db('MATCH (date) WHERE date.year = ' + str(year) + ' RETURN date')
+	result = query_db('MATCH (date)-[:HAS_CASES]->(case) WHERE date.year = "' + str(year) + '" RETURN case')
 	return form_json(result)
 
 @app.route('/case/<defendant>/<prosecutor>')
@@ -51,13 +53,11 @@ def def_cases(defendant_surname, defendant_name):
 def pros_cases(prosecutor_surname, prosecutor_name):
 	return form_json(query_cases_by_role('prosecutor', prosecutor_name, prosecutor_surname))
 
-@app.route('/cases/town/<town>')
-def town_cases(town):
-	return ''
-
 @app.route('/cases/judges/<judge_surname>/<judge_name>')
-def judge_name_cases(judge_surname, judge_name):
-	pass
+def judge_cases(judge_surname, judge_name):
+	query_str = 'MATCH (judge)-[:JUDGES]->(case) WHERE judge.j_surname =~ "' + judge_surname + '.*" AND judge.j_name =~ "' + judge_name + '.*" RETURN judge, case'
+	result = query_db(query_str)
+	return form_json(result)
 
 @app.errorhandler(404)
 def not_found(error):
